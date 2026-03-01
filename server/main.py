412a8991256c009a7355aacb7df52be3eb629329
syang1290 <stevey13@uci.edu>
2026-02-28 22:11:42 -0800

    chore: populate requirements.txt for deployment

--- server/main.py
+++ server/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="PlotWise AI Engine")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
